11baf42e4c7d9abf50aada6fd6ce5090d3b9897b
binary612 <sa0ka6mo1to2@gmail.com>
2023-11-23 23:14:09 +0800

    custom

---
+++ customization/customize.php
@@ -0,0 +1,70 @@
+<?php
+include_once "../layout/navbar.php";
+?>
+
+  <div class="customize-header"></div>
+  <div class="container-fluid">
+    <p class="text-customize-header ">球隊專屬客製化球桿服務</p>
+  </div>
+  <div class="container-fluid my-5">
+    <div class="row">
+      <div class="col-6">
+        <img src="../public/images/about03.png" alt="" class="img-fluid d-block mx-auto">
+      </div>
+      <div class="col-6">
+        <p class="text-customize-title">開始製作您的球隊專屬客製化球桿</p>
+        <p class="text-customize-title">簡單四步驟完成客製化球桿</p>
+        <div class="row mt-5">
+          <div class="col-1">
+            <div class="d-flex flex-column">
+              <div class="circle"></div>
+              <div class="rectangle"></div>
+            </div>
+          </div>
+          <div class="col-11">
+            <h3 class="text-white">第一步<br>與我們聯繫</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-1">
+            <div class="d-flex flex-column">
+              <div class="circle"></div>
+              <div class="rectangle"></div>
+            </div>
+          </div>
+          <div class="col-11">
+            <h3 class="text-white">第二步<br>與設計師討論，產出示意圖</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-1">
+            <div class="d-flex flex-column">
+              <div class="circle"></div>
+              <div class="rectangle"></div>
+            </div>
+          </div>
+          <div class="col-11">
+            <h3 class="text-white">第三步<br>定稿</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-1">
+            <div class="d-flex flex-column">
+              <div class="rectangle2"></div>
+              <div class="circle"></div>
+            </div>
+          </div>
+          <div class="col-8">
+            <h3 class="text-white">第四步<br>完成</h3>
+          </div>
+          <div class="col-3">
+            <button class="customize-button btn w-100"><a href="" class="text-decoration-none text-white">聯繫我們</a></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php
+require_once '../layout/footer.php';
+?>
